2f279e867979d4c73d8d95498d912c2498c0e92c
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-22 17:26:48 +0100

    aggiunte categorie

--- resources/views/molisana_home.blade.php
+++ resources/views/molisana_home.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="container">
+    @if(! empty($lunghe))
+    <section class="type-section container">
+        <h2>LE LUNGHE</h2>
         <div class="cards">
-            @foreach ($cards as $card)
+            @foreach ($lunghe as $card)
                 <div class="card">
                     <a href="">
                     <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}" srcset="">
@@ -13,4 +15,35 @@
             @endforeach
         </div>
     </section>
+    @endif
+    @if (! @empty($corte))
+        <section class="type-section container">
+        <h2>LE CORTE</h2>
+        <div class="cards">
+            @foreach ($corte as $card)
+                <div class="card">
+                    <a href="">
+                    <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}" srcset="">
+                    <h3>{{ $card['titolo']}}</h3>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </section>
+    @endif
+    @if(! empty($cortissime))
+    <section class="type-section container">
+        <h2>LE CORTISSIME</h2>
+        <div class="cards">
+            @foreach ($cortissime as $card)
+                <div class="card">
+                    <a href="">
+                    <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}" srcset="">
+                    <h3>{{ $card['titolo']}}</h3>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </section>
+    @endif
 @endsection
