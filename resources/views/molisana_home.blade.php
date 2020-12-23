93c8f2069aa3f5a91531ffa7f1f5eddeac190c10
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-23 14:53:56 +0100

    aggiunta pagina card singola pre tre tipi

--- resources/views/molisana_home.blade.php
+++ resources/views/molisana_home.blade.php
@@ -1,53 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-{{-- utilizzo if per capire se l'array utilizzato è vuoto in modo da non dover mostrare la sezione --}}
-    @if(! empty($lunghe))
-    <section class="type-section container">
-        <h2>LE LUNGHE</h2>
-        <div class="cards">
-            {{-- utilizzo foreach su array lunghe cioè prodotti con tipologia lunga  --}}
-            @foreach ($lunghe as $card)
-                <div class="card">
-                    <a href="{{ route('product', $card['id']) }}">
-                    <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}" srcset="">
-                    <h3>{{ $card['titolo']}}</h3>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    </section>
-    @endif
 
-    @if (! empty($corte))
-        <section class="type-section container">
-        <h2>LE CORTE</h2>
-        <div class="cards">
-            @foreach ($corte as $card)
-                <div class="card">
-                    <a href="{{ route('product', $card['id']) }}">
-                    <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}" srcset="">
-                    <h3>{{ $card['titolo']}}</h3>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    </section>
-    @endif
+    @include('partials.cards', ['cards' => $lunghe ,'title' => 'LE LUNGHE']);
+    @include('partials.cards', ['cards' => $corte ,'title' => 'LE CORTE']);
+    @include('partials.cards', ['cards' => $cortissime ,'title' => 'LE CORTISSIME']);
     
-    @if(! empty($cortissime))
-    <section class="type-section container">
-        <h2>LE CORTISSIME</h2>
-        <div class="cards">
-            @foreach ($cortissime as $card)
-                <div class="card">
-                    <a href="{{ route('product', $card['id']) }}">
-                    <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}" srcset="">
-                    <h3>{{ $card['titolo']}}</h3>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    </section>
-    @endif
 @endsection
